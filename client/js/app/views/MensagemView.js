37d0874ea12f03feaf114ce513044a03ae723702
Gabriel Napoleão <gabriel.ndr.sky@gmail.com>
2018-08-20 00:35:21 -0300

    Refactory code

--- client/js/app/views/MensagemView.js
+++ client/js/app/views/MensagemView.js
@@ -4,6 +4,6 @@ class MensagemView extends View{
 	}
 
 	template(model){
-    return model.texto ? `<p class="alert alert-info">${model.texto}</p>` : '<p></p>';
+	return model.texto ? `<p class="alert alert-info">${model.texto}</p>` : '<p></p>';
 	}
 }
