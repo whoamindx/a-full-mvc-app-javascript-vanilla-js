211791592f6c67b319550f1e73526ffa54a8434a
Gabriel Napoleão <gabriel.ndr.sky@gmail.com>
2018-09-05 20:38:39 -0300

    Refactoring code of ProxyFactory

--- client/js/app/services/ProxyFactory.js
+++ client/js/app/services/ProxyFactory.js
@@ -5,19 +5,18 @@ class ProxyFactory{
 		  get(target, prop, receiver) {
         if(props.includes(prop) && self._isFunction(target[prop])) {
           return function(){
-						console.log(`método '${prop}' interceptado`);
-						Reflect.apply(target[prop], target, arguments);
-						return acao(target);
+						console.log(`interceptando ${prop}`);
+						let retorno = Reflect.apply(target[prop], target, arguments);
+						acao(target);
+						return retorno;
     			}
 	    	}
 	    	return Reflect.get(target, prop, receiver);
 	  	},
 		  set(target, prop, value, receiver){
-		  	if (props.includes(prop)) {
-		  		acao(target);
-		  		target[prop] = value;
-		  	};		  	
-		  	return Reflect.set(target, prop, value, receiver);
+		  	let retorno = Reflect.set(target, prop, value, receiver);
+		    if(props.includes(prop)) acao(target);
+		    return retorno;
 		  }
 		});
 	}
